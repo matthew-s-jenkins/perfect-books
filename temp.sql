2549b2293c6481b40649d72150e0cf898d10b283
Matthew Jenkins <54869556+SemanticSalt-dev@users.noreply.github.com>
2025-10-09 23:21:08 -0400

    Folders reorganized.
    Bug fix - Edit amount of recurring income now allows
    the user to enter more than a single digit.

--- temp.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS `users` (
-  `user_id` int NOT NULL AUTO_INCREMENT,
-  `username` varchar(50) NOT NULL,
-  `password_hash` varchar(255) NOT NULL,
-  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`user_id`),
-  UNIQUE KEY `username` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3;
-
